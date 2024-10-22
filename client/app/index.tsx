e690bfe35bfe0736e25fead5c51fa76af962c74e
Noe Jacob <noecjacob02@gmail.com>
2024-10-23 09:51:41 +1100

    Fix all pantry issues

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -11,12 +11,17 @@ import OnboardScreen from './screens/OnboardScreen';
 import { useAuth} from '../components/AuthProvider';
 import HomeScreen from './screens/HomeScreen';
 
+import 'react-native-gesture-handler'; // Add this import at the top
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   const { isAuthenticated, loading } = useAuth();
 
   return (
+
+    <GestureHandlerRootView style={{ flex: 1 }}>
 
     <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{
       headerShown: false, // This will hide the header
@@ -30,6 +35,6 @@ export default function App() {
         {/* <Stack.Screen name="HealthProfileScreen" component={HealthProfileScreen} /> 
         <Stack.Screen name="HealthGoalsSelection" component={HealthGoalsSelection} /> */}
     </Stack.Navigator>
-
+    </GestureHandlerRootView>
   );
 }
